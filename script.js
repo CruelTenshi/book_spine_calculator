264e8627067e4b83b3fccd08be73d3aa7c48954e
CruelTenshi <99137401+CruelTenshi@users.noreply.github.com>
2025-02-16 13:07:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ function calculate() {
 	var page = parseFloat(document.getElementById('page').value);
 	var spine;
 	if (isNaN(page)) {
-		alert('정수를 입력해주세요.')
+		alert('양의 정수를 입력해주세요.')
 	} else if (!Number.isInteger(page)) {
-		alert('정수를 입력해주세요.')
+		alert('양의 정수를 입력해주세요.')
 	} else {
+		if(page < 1) {
+			alert('양의 정수를 입력해주세요.')
+		} else {
 		spine = Number((0.046*page).toFixed(1))/10;
-		document.getElementById('spine_width').innerText = spine.toFixed(2);
-		document.getElementById('width').innerText = Number(((6 + 210*2)/10 + spine).toFixed(2));
-		document.getElementById('height').innerText = 30.3;
-		document.getElementById('fromTop').innerText = 0.3;
-		document.getElementById('fromLeft').innerText = Number(((3 + 210)/10 + spine).toFixed(2));
+			document.getElementById('spine_width').innerText = spine.toFixed(1);
+			document.getElementById('width').innerText = Number(((6 + 210*2)/10 + spine).toFixed(2));
+			document.getElementById('height').innerText = 30.3;
+			document.getElementById('fromTop').innerText = 0.3;
+			document.getElementById('fromLeft').innerText = Number(((3 + 210)/10 + spine).toFixed(2));
+		}
 	}
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("page").addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault(); 
+            calculate();
+        }
+    });
+});
